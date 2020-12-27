ca7f90b59cf25057b39f29a0584606f0901818e7
Amine <kanzari.amin@gmail.com>
2020-12-27 11:23:36 +0100

    Managing users in RBAC

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
-
+const mongoose = require("mongoose");
 router.get("/users", async (req, res, next) => {
   try {
     const users = await User.find();
-    res.send(users);
+    //res.send(users);
+    res.render("manageUser", { users });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/user/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      req.flash("error", "Invalid ID");
+      res.redirect("/admin/users");
+      return;
+    }
+    const person = await User.findById(id);
+    res.render("profile", { person });
   } catch (error) {
     next(error);
   }
